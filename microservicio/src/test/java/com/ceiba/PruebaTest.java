bdecbca0c3919274b97e3a9bad2689baf0861786
santiago.betancur <santiago.betancur@SANTIAGO-BETANCUR.ceiba.local>
2021-07-07 21:46:05 -0500

    Se realiza prueba unitaria

---
+++ microservicio/src/test/java/com/ceiba/PruebaTest.java
@@ -0,0 +1,15 @@
+package com.ceiba;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PruebaTest {
+
+	@Test
+	void test() {
+		startsWith("Not yet implemented");
+	}
+
+}
